c95dd9ddf7701d2ed1dfb9b17ee27669477db16b
georgeGitM <zhiz2@student.unimelb.edu.au>
2018-02-25 21:34:17 +1100

    remove 1 hot

--- model.py
+++ model.py
@@ -41,7 +41,7 @@ class Model():
 
         with tf.variable_scope('rnn'):
             state_size = self.dim_embedding
-            inputs = tf.one_hot(self.X, 5000)
+            # inputs = tf.one_hot(self.X, 5000)
             def make_cell():
                 cell = tf.nn.rnn_cell.BasicLSTMCell(state_size)
                 # cell = tf.cond(self.keep_prob < 1, tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.keep_prob), cell)
